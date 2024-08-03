227e34bb4e8f2225f55a97d52eebb12c08518d24
Mitali Dixit <mitali.dixit04@gmail.com>
2024-08-03 21:42:10 +0530

    changed env variables

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZF61W_8XbEHre8XeH_a8LFT9ipD-SWN0",
-  authDomain: "inventory-management-16431.firebaseapp.com",
-  projectId: "inventory-management-16431",
-  storageBucket: "inventory-management-16431.appspot.com",
-  messagingSenderId: "209219415559",
-  appId: "1:209219415559:web:b87eecb41a5cdd338d04a2",
-  measurementId: "G-XRGKYBEQ24"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
